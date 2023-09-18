1ec3fe3d1db86af9df7a4b4774dc6310acee8258
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-18 18:40:31 +0530

    second

--- Inheritance/Animal.java
+++ Inheritance/Animal.java
@@ -1,5 +1,19 @@
 package Inheritance;
 
 public class Animal {
+
+    public String name = "Animal";
+    public String parts = "legs,tail";
+    public int age = 12;
+    private String generic = "Sciencetific generics";
+    
+    void eat(){
+        System.out.println("eationg every thing ");
+
+    }
+
+   private void walk(){
+        System.out.println("waliking using legs");
+    }
     
 }
---
+++ Inheritance/Dog.java
@@ -0,0 +1,21 @@
+package Inheritance;
+
+
+public class Dog extends Animal {
+
+    public static void main(String[] args) {
+        
+    }
+    
+    public void testMethod(){
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(parts);
+      //  System.out.println(generic);
+
+      eat();
+      //walk();
+
+    }
+    
+}
