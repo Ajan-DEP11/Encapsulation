19bdaca0bf734f730a50b7fea2dacccc889d00e7
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-18 19:37:34 +0530

    first

---
+++ Polimorphism/Demo.java
@@ -0,0 +1,25 @@
+package Polimorphism;
+
+public class Demo {
+
+    public void myMethod(){
+        System.out.println("run mymethod");
+
+    }
+    public void myMethod(int x){
+        System.out.println("run mymethod(int x)");
+
+    }
+    public void myMethod(String name){
+        System.out.println("run mymethod(String name)");
+
+    }
+    public void myMethod(int x,String naame){
+        System.out.println("run mymethod(int x,String name)");
+
+    }
+    
+
+
+    
+}
