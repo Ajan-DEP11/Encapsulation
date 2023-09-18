116bf3b9325aa8f9690b4fd1e58901d27a869f16
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-19 01:26:10 +0530

    third

--- Polimorphism/PolymorMain.java
+++ Polimorphism/PolymorMain.java
@@ -2,6 +2,8 @@ package Polimorphism;
 
 public class PolymorMain {
     public static void main(String[] args) {
+
+        //compile time polymorphism
        Demo demo = new Demo();
        demo.myMethod(2);
        demo.myMethod("Ajan");
