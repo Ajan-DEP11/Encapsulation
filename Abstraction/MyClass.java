ddf5deb3f6fb88a11796453d3a7613f81af5fd3a
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-19 01:44:43 +0530

    fourth

---
+++ Abstraction/MyClass.java
@@ -0,0 +1,12 @@
+package Abstraction;
+
+public interface MyClass {
+
+    public void myMethod();
+
+    public int calculateTotal(int num1, int num2);
+    
+    public void testMethod();
+
+    
+}
---
+++ Abstraction/MyImplimantation.java
@@ -0,0 +1,24 @@
+package Abstraction;
+
+public class MyImplimantation implements MyClass{
+
+    @Override
+    public void myMethod() {
+       
+        
+    }
+
+    @Override
+    public void testMethod() {
+        
+        
+    }
+
+
+    @Override
+    public int calculateTotal(int num1, int num2) {
+        int total = num1 + num2;
+        return total;
+      
+    }
+}
